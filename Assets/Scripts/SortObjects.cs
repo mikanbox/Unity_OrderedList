96cd8e355cc590a282eec1b0f756932fa1b82464
mikanbox <mikanbox@users.noreply.github.com>
2021-10-04 23:42:29 +0900

    modify sort

--- Assets/Scripts/SortObjects.cs
+++ Assets/Scripts/SortObjects.cs
@@ -7,6 +7,10 @@ using System;
 public class SortObjects : MonoBehaviour
 {
     public List<Transform> objlist = new List<Transform>(4);
+
+    bool isMoving = false;
+
+    public float time = 0f;
     
 
     //外部で決めた値を元にソート
@@ -18,29 +22,47 @@ public class SortObjects : MonoBehaviour
         }
     }
 
+    void Update() {
+        if (time > 0) {
+            // 移動中は座標を少しづつ変化させる
+        } else {
+            isMoving = false;
+        }
+    }
+
 
     // 内部の value を元にソート
     public void SortBasedInternalValue() {
-        List<Tuple<Transform,int>> objpairlist =  new List<Tuple<Transform,int>>();
+        if (isMoving)
+            return;
+        
+        List<Transform> tmpobjlist =  new List<Transform>();
         
         for(int i = 0; i < objlist[0].parent.childCount; i++ ) {
             Transform obj = objlist[0].parent.GetChild(i);
-            objpairlist.Add(new Tuple<Transform, int>(obj,obj.GetComponent<Value>().value ));
+            tmpobjlist.Add(obj);
         }
 
 
         // objpairlist.Sort((a, b) => a.Item2 - b.Item2 );
         // a.Item2 - b.Item2 < 0 なら a がbより前になる。つまり昇順ソート
-        for(int i = 0; i < objpairlist.Count ; i++ ) {
-            objpairlist[i].Item1.GetComponent<UnityEngine.UI.Image>().color = Color.black;
+        for(int i = 0; i < tmpobjlist.Count ; i++ ) {
+            tmpobjlist[i].GetComponent<UnityEngine.UI.Image>().color = Color.black;
         }
 
-        for(int i = 0; i < objpairlist.Count - 1; i++ ) {            
-            if (objpairlist[i].Item2 > objpairlist[i + 1].Item2 ) {
-                objpairlist[i].Item1.GetComponent<UnityEngine.UI.Image>().color = Color.blue;
-                objpairlist[i + 1].Item1.GetComponent<UnityEngine.UI.Image>().color = Color.blue;
+        for(int i = 0; i < tmpobjlist.Count - 1; i++ ) {       
+            int val_a = tmpobjlist[i].GetComponent<Value>().value;
+            int val_b = tmpobjlist[i + 1].GetComponent<Value>().value;
 
-                objpairlist[i + 1].Item1.SetSiblingIndex(i);
+
+            if (val_a > val_b) {
+                tmpobjlist[i].GetComponent<UnityEngine.UI.Image>().color = Color.blue;
+                tmpobjlist[i + 1].GetComponent<UnityEngine.UI.Image>().color = Color.blue;
+
+                // 切り替え後処理
+                tmpobjlist[i + 1].SetSiblingIndex(i);
+                time = 1f;
+                isMoving = true;
                 break;
             }
         }
